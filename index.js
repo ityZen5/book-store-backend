8b11b986d76e43d34d2596596463e18c0a057287
Bharath Raghavan <bharathrmofficial@gmail.com>
2024-10-22 18:09:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000
+require('dotenv').config()
+
+//middleware
+app.use(express.json())
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://vercel.com/bharath-raghavans-projects/book-store-frontend'],
+    credentials: true
+}))
+
+//routes
+const bookRoutes = require('./src/books/book.route')
+const orderRoutes = require('./src/orders/order.route')
+const userRoutes = require('./src/users/user.route')
+const adminRoutes = require("./src/stats/admin.stats")
+app.use("/api/books", bookRoutes)
+app.use("/api/orders", orderRoutes)
+app.use("/api/auth", userRoutes)
+app.use("/api/admin", adminRoutes)
+
+
+async function main() {
+    await mongoose.connect(process.env.DB_URL);
+    //tr8M4adqgsH1ysI9  bharathrmofficial
+    app.use('/', (req, res) => {
+    res.send("Welcome to Bharath's server!")
+    })
+}
+main().then(() => console.log("Mongdb connected successfully!")).catch(err => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
